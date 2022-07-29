f35f25823c6ca4835732366e7bb48beacd6bc74c
Camil Plojovic <camil.plojovic@levi9.com>
2022-07-29 18:15:41 +0200

    Added axios practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const options = {
+    method: "GET",
+    url: "https://numbersapi.p.rapidapi.com/7/24/date",
+    params: { fragment: "true", json: "true" },
+    headers: {
+      "X-RapidAPI-Key": "ac602fb9a7msh43d1a036da9742cp143ba0jsn02c2f5ba4c39",
+      "X-RapidAPI-Host": "numbersapi.p.rapidapi.com",
+    },
+  };
+
+  const [dateData, setDateData] = useState({});
+  const [inputedDate, setInputedDate] = useState("");
+
+  const fetchNumberData = async () => {
+    // axios.request(options).then(() => setDateData(res)).catch((err) => console.log(err));
+    const res = await axios.request(options).catch((err) => console.log(err));
+
+    setDateData(res);
+    console.log(res);
+  };
+
+  useEffect(() => {
+    fetchNumberData();
+  }, [dateData]);
+
+  const search = () => {
+    options.url = `https://numbersapi.p.rapidapi.com/${inputedDate}/date`;
+    fetchNumberData();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={(input) => setInputedDate(input.target.value)} />
+      <button onClick={search}>Search</button>
+      <h1>{dateData.data?.text}</h1>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/SearchInput.js
@@ -0,0 +1,3 @@
+const SearchInput = () => {
+  return <div></div>;
+};

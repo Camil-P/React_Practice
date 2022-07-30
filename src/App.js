1bcd3a7d9e9c3b33a9a528d6750e3d439614bd9b
Camil Plojovic <camil.plojovic@levi9.com>
2022-07-30 18:12:58 +0200

    child-parent comp

--- src/App.js
+++ src/App.js
@@ -1,42 +1,33 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import SearchInput from "./SearchInput";
 
 const App = () => {
-  const options = {
-    method: "GET",
-    url: "https://numbersapi.p.rapidapi.com/7/24/date",
-    params: { fragment: "true", json: "true" },
-    headers: {
-      "X-RapidAPI-Key": "ac602fb9a7msh43d1a036da9742cp143ba0jsn02c2f5ba4c39",
-      "X-RapidAPI-Host": "numbersapi.p.rapidapi.com",
-    },
+  let unetaVrednost = "";
+
+  const nekaFunc = () => {
+    return "Dobili smo sertifikate";
   };
 
-  const [dateData, setDateData] = useState({});
-  const [inputedDate, setInputedDate] = useState("");
-
-  const fetchNumberData = async () => {
-    // axios.request(options).then(() => setDateData(res)).catch((err) => console.log(err));
-    const res = await axios.request(options).catch((err) => console.log(err));
-
-    setDateData(res);
-    console.log(res);
+  const getInputedValue = (input, b) => {
+    unetaVrednost = input;
+    console.log(unetaVrednost, b);
   };
 
-  useEffect(() => {
-    fetchNumberData();
-  }, [dateData]);
-
-  const search = () => {
-    options.url = `https://numbersapi.p.rapidapi.com/${inputedDate}/date`;
-    fetchNumberData();
+  const konzologujText = () => {
+    console.log("HEJ TI NEPISMENI COVECE");
   };
+
+  const nekiString = { prop: "Prikaz stringa" };
 
   return (
     <div>
-      <input onChange={(input) => setInputedDate(input.target.value)} />
-      <button onClick={search}>Search</button>
-      <h1>{dateData.data?.text}</h1>
+      <SearchInput
+        getInput={getInputedValue}
+        buttonText="Nikola"
+        Camil={konzologujText}
+      ></SearchInput>
+
+      <h1 style={{ color: "blue", fontSize: "25px" }}>{nekaFunc()}</h1>
+      <p>{nekiString.prop}</p>
     </div>
   );
 };
--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,3 +1,14 @@
-const SearchInput = () => {
-  return <div></div>;
+const SearchInput = ({ Camil, buttonText, getInput }) => {
+  return (
+    <div>
+      <input
+        onChange={(el) => {
+          getInput(el.target.value, "nikola");
+        }}
+      ></input>
+      <button onClick={Camil}>{buttonText}</button>
+    </div>
+  );
 };
+
+export default SearchInput;

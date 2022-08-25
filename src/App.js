089ec1aaf42991a2e201188cff3a9667ca379903
Camil Plojovic <camil.plojovic@levi9.com>
2022-08-25 18:24:30 +0200

    Added mui app

--- src/App.js
+++ src/App.js
@@ -1,42 +1,64 @@
-import SearchInput from "./SearchInput";
-import { useState } from "react";
+import Header from "./Header";
+import { TextField, Grid, Button } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import DisplayCard from "./DisplayCard";
+
+const url = "https://api.github.com/users/";
 
 const App = () => {
-  const [inputedValue, setInputedValue] = useState("Daris");
-  const [inputedImage, setinputedImage] = useState(null);
-  console.log(inputedImage);
+  const inputRef = useRef();
+  const [searchedUser, setSearchedUser] = useState({
+    name: "",
+    bio: "",
+    avatar_url: "",
+  });
 
-  const nekaFunc = () => {
-    return "Dobili smo sertifikate";
+  const fetchUser = (user = "dzemildupljak") => {
+    axios
+      .get(url + user)
+      .then((res) => {
+        setSearchedUser(res.data);
+        console.log(res.data);
+      })
+      .catch((m) => console.log);
   };
 
-  const getInputedValue = (input) => {
-    setInputedValue(input);
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const changeUser = () => {
+    fetchUser(inputRef.current.value);
   };
-
-  const getImage = (image) => {
-    var reader = new FileReader();
-
-    reader.onload = function (event) {
-      setinputedImage(event.target.result);
-    };
-
-    reader.readAsDataURL(image);
-  };
-
-  const nekiString = { prop: "Prikaz stringa" };
 
   return (
     <div>
-      <SearchInput
-        getInput={getInputedValue}
-        buttonText="Nikola"
-        handleOnClick={getImage}
-      ></SearchInput>
-
-      <h1 style={{ color: "blue", fontSize: "25px" }}>{inputedValue}</h1>
-      <img src={inputedImage}></img>
-      <p>{nekiString.prop}</p>
+      <Header />
+      <Grid
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        container
+      >
+        <Grid item xs={2}>
+          <TextField
+            inputRef={inputRef}
+            placeholder="search..."
+            id="standard-basic"
+            label="Search"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Button onClick={changeUser} variant="contained">
+            Search
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <DisplayCard data={searchedUser} />
+        </Grid>
+      </Grid>
     </div>
   );
 };
